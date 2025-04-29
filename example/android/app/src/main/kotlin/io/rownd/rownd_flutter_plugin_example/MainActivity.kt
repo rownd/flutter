c316ad1a322b031144de91d76cea6e79b6f6a22c
Matt Hamann <mhamann@rownd.io>
2025-04-29 15:05:19 -0400

    chore: remove old example

--- example/android/app/src/main/kotlin/io/rownd/rownd_flutter_plugin_example/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package io.rownd.rownd_flutter_plugin_example
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
